b9de1815ebefd7481a53bfb5af455f963a82aa27
VictorN3021 <victorn3321@gmail.com>
2024-03-20 17:20:04 -0400

    Update Menu_pausa.cs

--- Assets/Scripts/Menu_pausa.cs
+++ Assets/Scripts/Menu_pausa.cs
@@ -1,18 +1,34 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class Menu_pausa : MonoBehaviour
+public class Menu_pausa : MonoBehaviourPun
 {
     public GameObject menupausa;
     public GameObject otro_Menu;
-    public GameObject player;
+    private GameObject[] players;
+
+    private void Start()
+    {
+        players = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject player in players)
+        {
+            if (photonView.IsMine && players != null)
+            {
+                GameObject playerlocal = player;
+            }
+        }
+    }
 
     private void Update()
     {
+        
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+           /* playerlocal.gameObject.GetComponent<Movimiento>().enabled = false;
+            playerlocal.gameObject.GetComponentInChildren<CamaraSet>().enabled = false;*/
             if (menupausa.gameObject.activeSelf == true)
             {
                 menupausa.gameObject.SetActive(false);
@@ -32,6 +48,8 @@ public class Menu_pausa : MonoBehaviour
         {
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
+            /*playerlocal.gameObject.GetComponent<Movimiento>().enabled = false;
+            playerlocal.gameObject.GetComponentInChildren<CamaraSet>().enabled = false;*/
         }
 
 
