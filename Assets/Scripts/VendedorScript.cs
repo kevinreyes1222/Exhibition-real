418e84a874805e19bcb7fec0c21b605be07383e6
Kevin Reyes <70299081+kevinreyes1222@users.noreply.github.com>
2024-03-20 05:07:41 -0400

    voleteria finalizada
    fue agregado un evento en la animacion

--- Assets/Scripts/VendedorScript.cs
+++ Assets/Scripts/VendedorScript.cs
@@ -1,27 +1,34 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class VendedorScript : MonoBehaviour
 {
-    public AudioClip audioClip; 
+    public AudioClip audioClip;
     private AudioSource audioSource;
     private bool clipPlayed = false;
-   
+
     private bool animationPlayed = false;
     public Animator animador;
+
+    public GameObject canvas;
+
+    public GameObject transportador;
     private void Start()
     {
-       
+        //transportador.GetComponent<MeshRenderer>();
         audioSource = GetComponent<AudioSource>();
-       
+
         audioSource.clip = audioClip;
+
+        
     }
 
- 
+
     private void OnTriggerEnter(Collider other)
     {
-        
+
         if (other.CompareTag("Player") && !clipPlayed)
         {
             // Reproducir el clip de audio
@@ -31,10 +38,29 @@ public class VendedorScript : MonoBehaviour
 
         if (!animationPlayed)
         {
-          
-            
+
+
             animationPlayed = true;
             animador.SetTrigger("talking");
+            animador.SetBool("notTalking", true);
         }
+
+        
+
+       
+    }
+    public void EncenderUi()
+    {
+        canvas.gameObject.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+    public void CerrarUi()
+    {
+        canvas.gameObject.SetActive(false);
+        transportador.GetComponent <MeshRenderer>().enabled = true;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
     }
 }
